68f9d7b2482e99c3ac3b0fde5f4933fd9d6ccdbf
pantic1801 <p.jovanica@hotmail.com>
2019-04-15 19:12:08 +0200

    importing project

---
+++ WebPantic/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebPantic.Models;
+
+namespace WebPantic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {         
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            
+           return View();
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
